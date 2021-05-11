4e9326e90c7680c68fc36b3b46d6bebae8271179
Austin Crim <crim.austin@principal.com>
2021-05-11 10:03:06 -0500

    add greeting

--- handler/main.go
+++ handler/main.go
@@ -14,7 +14,12 @@ func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 	for key, value := range request.Headers {
 		fmt.Printf("%s: %s\n", key, value)
 	}
-	return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 200}, nil
+
+	name := request.QueryStringParameters["name"]
+	if name == "" {
+		name = "Friend"
+	}
+	return events.APIGatewayProxyResponse{Body: fmt.Sprintf("Hello, %s!", name), StatusCode: 200}, nil
 }
 
 func main() {
